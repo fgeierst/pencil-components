8da01761e1769921c2444646cf8f2f98e67807d4
Florian Geierstanger <florian.geierstanger@codecentric.de>
2025-02-21 12:16:59 +0100

    test computed accessible name

--- src/components/pen-button/pen-button.e2e.ts
+++ src/components/pen-button/pen-button.e2e.ts
@@ -8,4 +8,13 @@ describe('pen-button', () => {
     const element = await page.find('pen-button');
     expect(element).toHaveClass('hydrated');
   });
+
+  it('has a accessible name', async () => {
+    const page = await newE2EPage();
+    await page.setContent('<pen-button>Click me</pen-button>');
+
+    const expectedAccessibleName = 'Click me';
+    const element = await page.find(`pen-button >>> button::-p-aria([name="${expectedAccessibleName}"])`);
+    expect(element).not.toBeNull();
+  });
 });
